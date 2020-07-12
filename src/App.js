a4197e644c073a6f9af0768a6191f1a44a8dc1ea
Queenie Smith <smith.queenie@gmail.com>
2020-07-11 22:11:56 -0400

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>NETFLIX</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/signin">
+          <p>SIGN IN PAGE</p>
+        </Route>
+        <Route path="/signup">
+          SIGN UP PAGE
+        </Route>
+        <Route path="/browse">
+          BROWSE PAGE
+        </Route>
+        <Route path="/">
+          HOME PAGE
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
