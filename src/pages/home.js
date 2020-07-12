a7e63b1a30a2565dd76368cca6ee5e9d92b3f403
Queenie Smith <smith.queenie@gmail.com>
2020-07-12 11:09:14 -0400

    Fix linter command

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,6 @@
 import React from "react";
-import Jumbotron from '../components/jumbotron';
+import Jumbotron from "../components/jumbotron";
 
 export default function Home() {
-  return (
-    <></>
-  )
+  return <></>;
 }
