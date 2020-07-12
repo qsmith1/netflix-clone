17b9a427c7f572cc225be561146e97afcf099936
Queenie Smith <smith.queenie@gmail.com>
2020-07-12 15:01:35 -0400

    Add global styles
    - Change export of components
    - Create GlobalStyles
    - Remove index.css
    - Add GlobalStyles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { GlobalStyles } from "./globalStyles";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
