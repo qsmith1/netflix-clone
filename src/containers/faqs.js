22afa1fe546058eaf8affcc21015cf0f848a642b
Queenie Smith <smith.queenie@gmail.com>
2020-07-12 21:16:15 -0400

    Create FAQs container

---
+++ src/containers/faqs.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Accordion } from '../components';
+
+export default function FaqsContainer() {
+  return (
+    <Accordion>
+      <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+    </Accordion>
+  );
+}
